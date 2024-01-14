9778ef950a0c1bbf91b5a26767dd5737cbcfcb3c
Himan Biswas <135246264+Himan145@users.noreply.github.com>
2024-01-14 16:41:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,11 @@ app.use(express.json({
     limit: '50mb'
     }));
 app.use(cors(
-    
+    {
+        origin:["https://p-kart-ecommerce.vercel.app"],
+        methods:["POST","PUT","DELETE","GET"],
+        credentials: true
+    }
 ));
 app.use(express.urlencoded({ extended: true, limit:'50mb' }));
 
